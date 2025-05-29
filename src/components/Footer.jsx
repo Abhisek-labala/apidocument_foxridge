2d28da6e8639adf3c2e5f3197f55be2fdcab086e
abhishek <abhishek.labla@ajatus.co.in>
2025-05-29 17:04:28 +0530

    added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
       <footer className="mt-3 text-center text-gray-500 text-sm pb-4">
         <p>
           © 2025 FoxRidge Payment Gateway • API Documentation • Last updated:
-          May 12, 2025
+          May 29, 2025
         </p>
       </footer>
     </div>
