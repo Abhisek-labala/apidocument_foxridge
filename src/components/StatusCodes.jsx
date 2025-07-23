f1e1422bbe7a5b58aca51b7a8c305196bf2b4b57
abhishek <abhishek.labla@ajatus.co.in>
2025-07-23 10:28:15 +0530

    added

--- src/components/StatusCodes.jsx
+++ src/components/StatusCodes.jsx
@@ -9,8 +9,8 @@ function StatusCodes() {
     { value: 1, type: 'string', status: 'Pending', description: 'The order is still waiting for user deposit' },
     { value: 2, type: 'string', status: 'Completed', description: 'User deposit successful, and the deposit amount matches' },
     // { value: 3, type: 'string', status: 'Amount Error', description: 'User deposit successful, but the deposit amount does not match, pending processing' },
-    { value: 4, type: 'string', status: 'Transaction Failed', description: 'Order rejected or cancelled' },
-    { value: 5, type: 'string', status: 'Expected Failure', description: 'Pending payment status for more than 1 day' },
+    { value: 3, type: 'string', status: 'Transaction Failed', description: 'Order rejected or cancelled' },
+    { value: 4, type: 'string', status: 'Expected Failure', description: 'Pending payment status for more than 1 day' },
     // { value: 6, type: 'string', status: 'Completed (Amount Error)', description: 'User deposit successful, but the deposit amount does not match' }
   ];
   
